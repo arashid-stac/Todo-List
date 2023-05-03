05c8de94a09cbc8aab14c6131241fbf3ccbbd0b4
Alli Rashid <allirashid97@gmail.com>
2023-05-03 17:58:27 -0300

    first commit

---
+++ functions.py
@@ -0,0 +1,19 @@
+FILEPATH = "todos.txt"
+
+
+def get_todo(filepath=FILEPATH):
+    """
+    Read a text file and return the list of
+    to-do items
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todo(todos_arg, filepath=FILEPATH):
+    """
+    Write the to-do items list in the text file
+    """
+    with open(FILEPATH, 'w') as file:
+        file.writelines(todos_arg)
